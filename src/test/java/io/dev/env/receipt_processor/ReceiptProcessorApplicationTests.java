6465c8bf64d19a46c3ae20b2065b27948c9caa70
cyrilsebastian1811 <sebastian.c@northeastern.edu>
2025-02-02 19:23:11 -0500

    Initializing project

---
+++ src/test/java/io/dev/env/receipt_processor/ReceiptProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dev.env.receipt_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
